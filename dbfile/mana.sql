006bff1888602b37e1960f21d50a8d6f3bce8aa8
towernter <tawandanyahuye.tn@gmail.com>
2019-09-02 16:26:44 +0200

    first commit

---
+++ dbfile/mana.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 02, 2019 at 04:22 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(13) NOT NULL,
+  `organisation` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `logged` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`name`, `surname`, `email`, `phone`, `organisation`, `password`, `logged`) VALUES
+('Sammuel', 'Kapenzi', 'sammy@gmail.com', 777777777, 'sammy', 'sammy', 1),
+('Tawanda', 'Nyahuye', 'tawandanyahuye@yahoo.com', 778612578, 'towernter', 'TAWAZ', 0),
+('Malvine', 'Chikochi', 'xixoxi@gmail.com', 89765, 'xixo', 'xixo', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email`
+--
+
+CREATE TABLE `email` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `email`
+--
+
+INSERT INTO `email` (`name`, `email`, `message`) VALUES
+('Tawanda', 'tawandanyahuye.tn@gmail.com', 'hello');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hire`
+--
+
+CREATE TABLE `hire` (
+  `car_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `destination` varchar(50) NOT NULL,
+  `hired` tinyint(1) NOT NULL,
+  `cost` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hire`
+--
+
+INSERT INTO `hire` (`car_name`, `email`, `destination`, `hired`, `cost`) VALUES
+('hammer', 'tawandanyahuye@yahoo.com', 'Chinhoyi', 1, 600),
+('limousine', 'tawandanyahuye@yahoo.com', 'Masvingo', 1, 960);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image`, `created`) VALUES
+(42, 'maxresdefault.jpg.2', '2018-05-16 17:34:13'),
+(43, '2e.jpg', '2018-05-16 17:35:34'),
+(44, '1e.jpg', '2018-05-16 17:36:48');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
